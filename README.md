18fa7356b8892114324f0e39bb7ad611a475b250
Abid <abid.aslam55570@gmail.com>
2024-05-26 19:44:58 +0530

    EDA completed

---
+++ README.md
@@ -0,0 +1,5 @@
+conda create -p env python=3.8 -y
+
+conda activate ./env
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "src",
+    version = "0.0.0.1",
+    aurthor = "Abid",
+    aurthor_emai = "abid.aslam55570@gmail.com",
+    packages = find_packages(),
+    install_requires = [],
+)
