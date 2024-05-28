8f6f4b82ef5adf8cb79416ab8407560b16eeda0f
Abid <abid.aslam55570@gmail.com>
2024-05-28 11:39:05 +0530

    prediction commit

--- src/components/constant/__init__.py
+++
@@ -1,3 +0,0 @@
-AWS_S3_BUCKET_NAME = "sensor-deployment"
-MONGO_DATABASE_NAME = "abid55570"
-MONGO_COLLECTION_NAME= "wafers_database"
---
+++ src/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "sensor-deployment"
+MONGO_DATABASE_NAME = "abid55570"
+MONGO_COLLECTION_NAME= "wafers_database"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://abid55570:test1234@cluster-wafer.qnalkgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-wafer"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
